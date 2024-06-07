ea54a93022bb17b493278e89fe15c370b7d6c8f8
o.galyaeva <lesskiig00@yandex.ru>
2024-06-07 11:20:51 +0300

    Added user dialog class, console variant of interface interactor, pass all tests successfull

---
+++ console_user_interface.py
@@ -0,0 +1,8 @@
+class ConsoleUserInterface:
+    @staticmethod
+    def input(message):
+        return input(message)
+
+    @staticmethod
+    def print(message):
+        print(message)
---
+++ user_dialog.py
@@ -0,0 +1,27 @@
+class UserDialog:
+    _COMMANDS = {
+        'get_status': 'показать статус',
+        'discharge': 'выписать',
+        'status_down': 'понизить статус'
+    }
+
+    def __init__(self, user_interface):
+        self._user_interface = user_interface
+
+    def _convert_patient_id(self, user_input_patient_id):
+        return int(user_input_patient_id)
+
+    def _convert_command(self, user_input_command):
+        for command, user_input in self._COMMANDS.items():
+            if user_input_command == user_input:
+                return command
+        return 'unknown'
+
+    def request_command(self):
+        user_input = self._user_interface.input("Введите команду: ")
+        return self._convert_command(user_input)
+
+    def request_patient_id(self):
+        user_input = self._user_interface.input("Введите id пациента: ")
+        return self._convert_patient_id(user_input)
+
