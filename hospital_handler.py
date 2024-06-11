718a2d530bf6acc82920d65fbc6a07fecdd9ed11
o.galyaeva <lesskiig00@yandex.ru>
2024-06-11 12:34:35 +0300

    Add invalid patient id case in status down method

--- hospital_handler.py
+++ hospital_handler.py
@@ -20,7 +20,7 @@ class HospitalHandler:
             self._patients_repository.status_down(patient_id)
             new_status = self._patients_repository.get_status(patient_id)
             self._user_dialog.print_status_changed(new_status)
-        except PatientNotExistsException as ex:
+        except (PatientNotExistsException, InvalidPatientIdException) as ex:
             self._user_dialog.print_message(ex.message)
 
     def discharge(self):
