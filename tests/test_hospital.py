0909c07b3bc4a6f26fdd5471df79212b394dca3e
o.galyaeva <lesskiig00@yandex.ru>
2024-06-11 15:25:04 +0300

    Add discharge e2e test

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -60,3 +60,33 @@ def test_status_down():
         ],
         any_order=False
     )
+    assert patients_repository._patients_base == [1, 3, 1, 0]
+
+
+def test_discharge():
+    patients_repository = PatientsRepository([2, 3, 1, 0])
+    console_mock = MagicMock()
+    user_dialog = UserDialog(console_mock)
+    hospital_handler = HospitalHandler(user_dialog, patients_repository)
+    hospital = Hospital(hospital_handler, user_dialog)
+
+    console_mock.input.side_effect = ['выписать', '1', 'стоп']
+
+    hospital.start_work()
+
+    console_mock.input.assert_has_calls(
+        [
+            call("Введите команду: "),
+            call("Введите id пациента: "),
+            call("Введите команду: "),
+        ],
+        any_order=False,
+    )
+    console_mock.print.assert_has_calls(
+        [
+            call('Пациент выписан'),
+            call('Работа закончена')
+        ],
+        any_order=False
+    )
+    assert patients_repository._patients_base == [3, 1, 0]
