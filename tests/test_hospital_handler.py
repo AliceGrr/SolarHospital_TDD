126118daffe247a56aea33f7c12a0b998c8b3995
o.galyaeva <lesskiig00@yandex.ru>
2024-06-11 11:51:24 +0300

    Add status_down test for hospital handler

--- tests/test_hospital_handler.py
+++ tests/test_hospital_handler.py
@@ -13,3 +13,15 @@ def test_get_status():
     hospital_handler.get_status()
 
     user_dialog.print_status.assert_called_with('Слегка болен')
+
+
+def test_status_down():
+    patients_repository = PatientsRepository([2, 3, 1, 0])
+    user_dialog = MagicMock()
+    user_dialog.input_patient_id = MagicMock(return_value=1)
+    hospital_handler = HospitalHandler(user_dialog, patients_repository)
+
+    hospital_handler.status_down()
+
+    user_dialog.print_status_changed.assert_called_with('Болен')
+    assert patients_repository._patients_base == [2, 2, 1, 0]
