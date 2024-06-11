033e6813fd821518e414a1cfc0b19246b58332fe
o.galyaeva <lesskiig00@yandex.ru>
2024-06-11 11:56:17 +0300

    Add discharge function to HospitalHandler

--- hospital_handler.py
+++ hospital_handler.py
@@ -13,3 +13,8 @@ class HospitalHandler:
         self._patients_repository.status_down(patient_id)
         new_status = self._patients_repository.get_status(patient_id)
         self._user_dialog.print_status_changed(new_status)
+
+    def discharge(self):
+        patient_id = self._user_dialog.input_patient_id()
+        self._patients_repository.discharge(patient_id)
+        self._user_dialog.print_patient_discharged()
