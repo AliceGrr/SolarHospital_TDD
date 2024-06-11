a751cf4ff44472ddc551860512ac7d329a80d63b
o.galyaeva <lesskiig00@yandex.ru>
2024-06-11 15:29:00 +0300

    Add new e2e tests (with errors)

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -34,6 +34,62 @@ def test_get_status():
     )
 
 
+def test_get_status_when_patient_not_exists():
+    patients_repository = PatientsRepository([2, 3, 1, 0])
+    console_mock = MagicMock()
+    user_dialog = UserDialog(console_mock)
+    hospital_handler = HospitalHandler(user_dialog, patients_repository)
+    hospital = Hospital(hospital_handler, user_dialog)
+
+    console_mock.input.side_effect = ['показать статус', '99', 'стоп']
+
+    hospital.start_work()
+
+    console_mock.input.assert_has_calls(
+        [
+            call("Введите команду: "),
+            call("Введите id пациента: "),
+            call("Введите команду: "),
+        ],
+        any_order=False,
+    )
+    console_mock.print.assert_has_calls(
+        [
+            call('Такого пациента нет в больнице'),
+            call('Работа закончена')
+        ],
+        any_order=False
+    )
+
+
+def test_get_status_when_string_patient_id():
+    patients_repository = PatientsRepository([2, 3, 1, 0])
+    console_mock = MagicMock()
+    user_dialog = UserDialog(console_mock)
+    hospital_handler = HospitalHandler(user_dialog, patients_repository)
+    hospital = Hospital(hospital_handler, user_dialog)
+
+    console_mock.input.side_effect = ['показать статус', 'два', 'стоп']
+
+    hospital.start_work()
+
+    console_mock.input.assert_has_calls(
+        [
+            call("Введите команду: "),
+            call("Введите id пациента: "),
+            call("Введите команду: "),
+        ],
+        any_order=False,
+    )
+    console_mock.print.assert_has_calls(
+        [
+            call('ID пациента должно быть положительным числом'),
+            call('Работа закончена')
+        ],
+        any_order=False
+    )
+
+
 def test_status_down():
     patients_repository = PatientsRepository([2, 3, 1, 0])
     console_mock = MagicMock()
@@ -61,6 +117,35 @@ def test_status_down():
         any_order=False
     )
     assert patients_repository._patients_base == [1, 3, 1, 0]
+
+
+def test_status_down_when_patient_stats_minimum():
+    patients_repository = PatientsRepository([2, 3, 1, 0])
+    console_mock = MagicMock()
+    user_dialog = UserDialog(console_mock)
+    hospital_handler = HospitalHandler(user_dialog, patients_repository)
+    hospital = Hospital(hospital_handler, user_dialog)
+
+    console_mock.input.side_effect = ['понизить статус', '4', 'стоп']
+
+    hospital.start_work()
+
+    console_mock.input.assert_has_calls(
+        [
+            call("Введите команду: "),
+            call("Введите id пациента: "),
+            call("Введите команду: "),
+        ],
+        any_order=False,
+    )
+    console_mock.print.assert_has_calls(
+        [
+            call('Статус нельзя понизить (никто не умирает у нас)'),
+            call('Работа закончена')
+        ],
+        any_order=False
+    )
+    assert patients_repository._patients_base == [2, 3, 1, 0]
 
 
 def test_discharge():
